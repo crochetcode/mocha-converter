558fcf39dd99777c790004571bea02a662efa320
fdavison1 <fred.noreply@gmail.com>
2020-01-17 14:30:26 -0600

    converter tests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mocha-converter
+https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
+
+"Mocha is a testing library for Node.js, created to be a simple, extensible, and fast testing suite. It’s used for unit and integration testing, and it’s a great candidate for BDD (Behavior Driven Development).
+
+This article will walk you through its installation and configuration, as well as demonstrate its usage by implementing a color converter that can translate from RGB (red, green, blue) to hexadecimal color representation. In addition, we will create a web server that exposes the above converter, and demonstrates how to test asynchronous code."
---
+++ test/converter.js
@@ -0,0 +1,27 @@
+const expect = require("chai").expect;
+const converter = require("../app/converter")
+
+describe("Color Code Converter", function() {
+    describe("RGB to Hex conversion", function(){
+        it("converts the basic colors", function(){
+            const redHex = converter.rgbToHex(255, 0, 0);
+            const greenHex = converter.rgbToHex(0, 255, 0);
+            const blueHex = converter.rgbToHex(0, 0, 255); 
+
+            expect(redHex).to.equal('ff0000');
+            expect(greenHex).to.equal('00ff00');
+            expect(blueHex).to.equal('0000ff');
+        });
+    });
+    describe("Hex to RGB Conversion", function(){
+        it("converts the basic colors", function(){
+            const red = converter.hextoRGB('ff0000');
+            const green = converter.hextoRGB('00ff00');
+            const blue = converter.hextoRGB('0000ff');
+
+            expect(red).to.deep.equal([255, 0, 0]);
+            expect(green).to.deep.equal([0, 255, 0]);
+            expect(blue).to.deep.equal([0, 0, 255]);
+        });
+    });
+});
